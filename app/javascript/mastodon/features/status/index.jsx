1fc105ad2824651634ff2e5d55591f1a10a985b2
Travis Briggs <audiodude@gmail.com>
2023-10-22 18:37:42 -0700

    Do not use local component, inline TimelineHint

--- app/javascript/mastodon/features/status/index.jsx
+++ app/javascript/mastodon/features/status/index.jsx
@@ -188,14 +188,6 @@ const titleFromStatus = (intl, status) => {
   return text ? `${user}: "${truncate(text, 30)}"` : intl.formatMessage(messages.statusTitleWithAttachments, { user, attachmentCount });
 };
 
-const RemoteHint = ({ url }) => (
-  <TimelineHint url={url} resource={<FormattedMessage id='timeline_hint.resources.replies' defaultMessage='Some replies' />} />
-);
-
-RemoteHint.propTypes = {
-  url: PropTypes.string.isRequired,
-};
-
 class Status extends ImmutablePureComponent {
 
   static contextTypes = {
@@ -676,7 +668,7 @@ class Status extends ImmutablePureComponent {
     const isIndexable = !status.getIn(['account', 'noindex']);
 
     if (!isLocal) {
-      remoteHint = <RemoteHint url={status.get('url')} />
+      remoteHint = <TimelineHint url={status.get('url')} resource={<FormattedMessage id='timeline_hint.resources.replies' defaultMessage='Some replies' />} />
     }
 
     const handlers = {
