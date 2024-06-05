280da954ee8282ca7a3c8d5a339889c04561823d
Eugen Rochko <eugen@zeonfederated.com>
2024-06-05 12:29:43 +0200

    WIP

---
+++ app/javascript/mastodon/features/notifications_v2/components/notification_admin_report.tsx
@@ -0,0 +1,7 @@
+import type { NotificationGroupAdminReport } from 'mastodon/models/notification_group';
+
+export const NotificationAdminReport: React.FC<{
+  notification: NotificationGroupAdminReport;
+}> = ({ notification }) => {
+  return null;
+};
