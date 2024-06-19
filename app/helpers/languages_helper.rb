a7e8e6d27a539fbf14173075214b65e095592a44
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-06-20 01:29:08 +0300

    update languages_helper.rb

--- app/helpers/languages_helper.rb
+++ app/helpers/languages_helper.rb
@@ -7,32 +7,7 @@ module LanguagesHelper
   }.freeze
 
   ISO_639_3 = {
-    ast: ['Asturian', 'Asturianu'].freeze,
-    chr: ['Cherokee', 'ᏣᎳᎩ ᎦᏬᏂᎯᏍᏗ'].freeze,
-    ckb: ['Sorani (Kurdish)', 'سۆرانی'].freeze,
-    cnr: ['Montenegrin', 'crnogorski'].freeze,
-    jbo: ['Lojban', 'la .lojban.'].freeze,
-    kab: ['Kabyle', 'Taqbaylit'].freeze,
-    ldn: ['Láadan', 'Láadan'].freeze,
-    lfn: ['Lingua Franca Nova', 'lingua franca nova'].freeze,
-    sco: ['Scots', 'Scots'].freeze,
-    sma: ['Southern Sami', 'Åarjelsaemien Gïele'].freeze,
-    smj: ['Lule Sami', 'Julevsámegiella'].freeze,
-    szl: ['Silesian', 'ślůnsko godka'].freeze,
-    tok: ['Toki Pona', 'toki pona'].freeze,
-    xal: ['Kalmyk', 'Хальмг келн'].freeze,
-    zba: ['Balaibalan', 'باليبلن'].freeze,
-    zgh: ['Standard Moroccan Tamazight', 'ⵜⴰⵎⴰⵣⵉⵖⵜ'].freeze,
-  }.freeze
-
-  # e.g. For Chinese, which is not a language,
-  # but a language family in spite of sharing the main locale code
-  # We need to be able to filter these
-  ISO_639_1_REGIONAL = {
-    'zh-CN': ['Chinese (China)', '简体中文'].freeze,
-    'zh-HK': ['Chinese (Hong Kong)', '繁體中文（香港）'].freeze,
-    'zh-TW': ['Chinese (Taiwan)', '繁體中文（臺灣）'].freeze,
-    'zh-YUE': ['Cantonese', '廣東話'].freeze,
+    
   }.freeze
 
   SUPPORTED_LOCALES = {}.merge(ISO_639_1).merge(ISO_639_1_REGIONAL).merge(ISO_639_3).freeze
