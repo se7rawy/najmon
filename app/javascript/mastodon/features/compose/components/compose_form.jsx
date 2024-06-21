c339e95d0ec2c4bc0d4f603d88dc000f92a9e1ec
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-06-22 00:19:08 +0300

    update compose_form.jsx

--- app/javascript/mastodon/features/compose/components/compose_form.jsx
+++ app/javascript/mastodon/features/compose/components/compose_form.jsx
@@ -285,7 +285,7 @@ class ComposeForm extends ImmutablePureComponent {
               <PollFormContainer />
             </div>
           </AutosuggestTextarea>
-          <EmojiPickerDropdown onPickEmoji={this.handleEmojiPick} />
+       
 
           <div className='compose-form__buttons-wrapper'>
             <div className='compose-form__buttons'>
