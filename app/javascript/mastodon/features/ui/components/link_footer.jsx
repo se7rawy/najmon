831042b977b905ab22dc3972f40ed76f27795f4a
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-06-27 09:57:44 +0300

    update link_footer.jsx

--- app/javascript/mastodon/features/ui/components/link_footer.jsx
+++ app/javascript/mastodon/features/ui/components/link_footer.jsx
@@ -64,7 +64,7 @@ class LinkFooter extends PureComponent {
     return (
       <div className='link-footer'>
         <p>
-          <strong>{domain}</strong>:
+     
           {' '}
           <Link to='/about' target={multiColumn ? '_blank' : undefined}><FormattedMessage id='footer.about' defaultMessage='About' /></Link>
           {statusPageUrl && (
