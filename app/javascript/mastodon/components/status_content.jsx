ba47e4e2d9588fcff6797e37814e75dc831c210d
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-02 22:54:56 +0300

    update status_content.jsx

--- app/javascript/mastodon/components/status_content.jsx
+++ app/javascript/mastodon/components/status_content.jsx
@@ -14,7 +14,7 @@ import PollContainer from 'mastodon/containers/poll_container';
 import { autoPlayGif, languages as preloadedLanguages } from 'mastodon/initial_state';
 
 const MAX_HEIGHT = 706; // 22px * 32 (+ 2px padding at the top)
-
+const MAX_CHARACTERS = 200;
 /**
  *
  * @param {any} status
@@ -243,7 +243,15 @@ class StatusContent extends PureComponent {
     const targetLanguages = this.props.languages?.get(status.get('language') || 'und');
     const renderTranslate = this.props.onTranslate && this.context.identity.signedIn && ['public', 'unlisted'].includes(status.get('visibility')) && status.get('search_index').trim().length > 0 && targetLanguages?.includes(contentLocale);
 
-    const content = { __html: statusContent ?? getStatusContent(status) };
+    let content = statusContent ?? getStatusContent(status);
+    const isContentTooLong = content.length > MAX_CHARACTERS;
+
+    if (isContentTooLong && hidden) {
+      content = content.substring(0, MAX_CHARACTERS) + '...';
+    }
+
+    const contentHtml = { __html: content };
+    
     const spoilerContent = { __html: status.getIn(['translation', 'spoilerHtml']) || status.get('spoilerHtml') };
     const language = status.getIn(['translation', 'language']) || status.get('language');
     const classNames = classnames('status__content', {
