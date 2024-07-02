d5f3430c6e55134c77426a1db2cbc8f8a6435ecb
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-02 23:48:24 +0300

    update status_content.jsx

--- app/javascript/mastodon/components/status_content.jsx
+++ app/javascript/mastodon/components/status_content.jsx
@@ -261,13 +261,9 @@ let contentHtml = statusContent ?? getStatusContent(status);
       'status__content--collapsed': renderReadMore,
     });
 
-const readMoreButton2 = isContentTooLong  && (
-      <button className='status__content__read-more-button' onClick={() => this.setState({ hidden: false })} key='read-more'>
-        <FormattedMessage id='status.read_more' defaultMessage='Read more' /><Icon id='angle-right' fixedWidth />
-</button>
-    );
+
     
-    const readMoreButton = renderReadMore && (
+    const readMoreButton = isContentTooLong && renderReadMore && (
       <button className='status__content__read-more-button' onClick={this.props.onClick} key='read-more'>
         <FormattedMessage id='status.read_more' defaultMessage='Read more' /><Icon id='angle-right' fixedWidth />
       </button>
