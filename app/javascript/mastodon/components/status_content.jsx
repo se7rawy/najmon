8fccd5e45aa7ee687abca97d0ddedd499a13e3a6
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-03 07:38:27 +0300

    update status_content.jsx

--- app/javascript/mastodon/components/status_content.jsx
+++ app/javascript/mastodon/components/status_content.jsx
@@ -324,7 +324,7 @@ let contentHtml = statusContent ?? getStatusContent(status);
             {translateButton}
           </div>
 
-    {readMoreButton}
+  //  {readMoreButton}
         </>
       );
     } else {
