843ab7dff5b7a481aa425ddf5b6998cce23fb151
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-07 07:25:48 +0300

    update status_content.jsx

--- app/javascript/mastodon/components/status_content.jsx
+++ app/javascript/mastodon/components/status_content.jsx
@@ -320,6 +320,7 @@ let contentHtml = statusContent ?? getStatusContent(status);
           <div className={classNames} ref={this.setRef} tabIndex={0} onMouseDown={this.handleMouseDown} onMouseUp={this.handleMouseUp} key='status-content' onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
             <div className='status__content__text status__content__text--visible translate' lang={language} dangerouslySetInnerHTML={content} />
   
+  
             {poll}
             {translateButton}
           </div>
