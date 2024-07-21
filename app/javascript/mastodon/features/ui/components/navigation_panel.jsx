79b29189e3a58c456f81fcbc09976cb668ff43eb
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-21 11:07:26 +0300

    update navigation_panel.jsx

--- app/javascript/mastodon/features/ui/components/navigation_panel.jsx
+++ app/javascript/mastodon/features/ui/components/navigation_panel.jsx
@@ -104,6 +104,8 @@ class NavigationPanel extends Component {
 
         {signedIn && (
           <>
+             <ColumnLink transparent href='/settings/preferences' icon='cog' text={intl.formatMessage(messages.preferences)} />
+                    <hr />
             <ColumnLink transparent to='/conversations' icon='at' text={intl.formatMessage(messages.direct)} />
             <ColumnLink transparent to='/bookmarks' icon='bookmark' text={intl.formatMessage(messages.bookmarks)} />
             <ColumnLink transparent to='/favourites' icon='star' text={intl.formatMessage(messages.favourites)} />
@@ -111,15 +113,13 @@ class NavigationPanel extends Component {
 
             <ListPanel />
 
-            <hr />
-
-            <ColumnLink transparent href='/settings/preferences' icon='cog' text={intl.formatMessage(messages.preferences)} />
+    
           </>
         )}
 
         <div className='navigation-panel__legal'>
           <hr />
-          <ColumnLink transparent to='/about' icon='ellipsis-h' text={intl.formatMessage(messages.about)} />
+          <ColumnLink transparent to='/about' icon='ellipsis-h'min-height: calc(100vh - $ui-header-height); text={intl.formatMessage(messages.about)} />
         </div>
 
         <NavigationPortal />
