d05846c765b6267c9cde703f098231fb6f4b337a
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-21 06:40:00 +0300

    update columns_area.jsx

--- app/javascript/mastodon/features/ui/components/columns_area.jsx
+++ app/javascript/mastodon/features/ui/components/columns_area.jsx
@@ -156,7 +156,10 @@ export default class ColumnsArea extends ImmutablePureComponent {
             </div>
           </div>
 
-
+          <div className='columns-area__panels__main'>
+            <div className='tabs-bar__wrapper'><div id='tabs-bar__portal' /></div>
+            <div className='columns-area columns-area--mobile'>{children}</div>
+          </div>
 
     
         </div>
