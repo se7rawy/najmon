1eed24391d41970067d77ec87b1a61690e849fba
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-21 07:40:04 +0300

    update columns_area.jsx

--- app/javascript/mastodon/features/ui/components/columns_area.jsx
+++ app/javascript/mastodon/features/ui/components/columns_area.jsx
@@ -151,9 +151,7 @@ export default class ColumnsArea extends ImmutablePureComponent {
           </div>
 
       <div className='columns-area__panels__pane columns-area__panels__pane--start columns-area__panels__pane--navigational'>
-            <div className='columns-area__panels__pane__inner'>
-              <NavigationPanel />
-            </div>
+      
           </div>
 
           <div className='columns-area__panels__main'>
