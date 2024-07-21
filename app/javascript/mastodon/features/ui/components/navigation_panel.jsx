954f129a4ce7f03b8ddb63cef472e044d563287e
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-21 11:13:23 +0300

    update navigation_panel.jsx

--- app/javascript/mastodon/features/ui/components/navigation_panel.jsx
+++ app/javascript/mastodon/features/ui/components/navigation_panel.jsx
@@ -104,8 +104,10 @@ class NavigationPanel extends Component {
 
         {signedIn && (
           <>
-             <ColumnLink transparent href='/settings/preferences' icon='cog' text={intl.formatMessage(messages.preferences)} />
-                    <hr />
+                      <hr />
+
+            <ColumnLink transparent href='/settings/preferences' icon='cog' text={intl.formatMessage(messages.preferences)} />
+            
             <ColumnLink transparent to='/conversations' icon='at' text={intl.formatMessage(messages.direct)} />
             <ColumnLink transparent to='/bookmarks' icon='bookmark' text={intl.formatMessage(messages.bookmarks)} />
             <ColumnLink transparent to='/favourites' icon='star' text={intl.formatMessage(messages.favourites)} />
@@ -113,7 +115,7 @@ class NavigationPanel extends Component {
 
             <ListPanel />
 
-    
+
           </>
         )}
 
