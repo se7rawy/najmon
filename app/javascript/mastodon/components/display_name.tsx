bb3048bb9a63bab482f54009ca2e60b52e859067
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-22 07:10:43 +0300

    update display_name.tsx

--- app/javascript/mastodon/components/display_name.tsx
+++ app/javascript/mastodon/components/display_name.tsx
@@ -114,7 +114,7 @@ export class DisplayName extends React.PureComponent<Props> {
         onMouseEnter={this.handleMouseEnter}
         onMouseLeave={this.handleMouseLeave}
       >
-        {displayName} {suffix}
+        {displayName} <br>{suffix}
       </span>
     );
   }
