7e08700e78b18d4a3717e87e48462dc65b7c283c
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-26 10:16:06 +0300

    update poll_form.jsx

--- app/javascript/mastodon/features/compose/components/poll_form.jsx
+++ app/javascript/mastodon/features/compose/components/poll_form.jsx
@@ -167,7 +167,7 @@ class PollForm extends ImmutablePureComponent {
           <button type='button' disabled={options.size >= 4} className='button button-secondary' onClick={this.handleAddOption}><Icon id='plus' /> <FormattedMessage {...messages.add_option} /></button>
 
           {/* eslint-disable-next-line jsx-a11y/no-onchange */}
-          <select value={expiresIn} onChange={this.handleSelectDuration}>
+          <select className='rtl_' value={expiresIn} onChange={this.handleSelectDuration}>
             <option value={300}>{intl.formatMessage(messages.minutes, { number: 5 })}</option>
             <option value={1800}>{intl.formatMessage(messages.minutes, { number: 30 })}</option>
             <option value={3600}>{intl.formatMessage(messages.hours, { number: 1 })}</option>
