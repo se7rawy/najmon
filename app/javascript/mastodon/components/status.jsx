ccdb37a3f60f776f12a1e2b353d03e94587f30c4
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-26 06:44:13 +0300

    update status.jsx

--- app/javascript/mastodon/components/status.jsx
+++ app/javascript/mastodon/components/status.jsx
@@ -437,7 +437,7 @@ class Status extends ImmutablePureComponent {
 
       prepend = (
         <div className='status__prepend'>
-          <div className='status__prepend-icon-wrapper'><Icon id='comment' className='status__prepend-icon' fixedWidth /></div>
+          <div className='status__prepend-icon-wrapper'><Icon id='reply' className='status__prepend-icon' fixedWidth /></div>
           <FormattedMessage id='status.replied_to' defaultMessage='Replied to {name}' values={{ name: <a onClick={this.handlePrependAccountClick} data-id={status.getIn(['account', 'id'])} href={`/@${status.getIn(['account', 'acct'])}`} className='status__display-name muted'><bdi><strong dangerouslySetInnerHTML={display_name_html} /></bdi></a> }} />
         </div>
       );
