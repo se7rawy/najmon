271e7f0dd1fa4e52ee1067caa474fcd2d708b97e
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-30 01:53:59 +0300

    update header.jsx

--- app/javascript/mastodon/features/ui/components/header.jsx
+++ app/javascript/mastodon/features/ui/components/header.jsx
@@ -17,7 +17,7 @@ import { registrationsOpen, me, sso_redirect } from 'mastodon/initial_state';
 const Account = connect(state => ({
   account: state.getIn(['accounts', me]),
 }))(({ account }) => (
-  <Link to='/settings/preferences' t title={account.get('acct')}>
+  <Link to='/settings/preferences' title={account.get('acct')}>
     <Avatar account={account} size={35} />
   </Link>
 ));
