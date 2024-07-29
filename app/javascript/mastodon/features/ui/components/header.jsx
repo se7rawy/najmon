52f7dec8f0bb6ba1cb0d42bbfba7df06f352613a
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-30 01:18:20 +0300

    update header.jsx

--- app/javascript/mastodon/features/ui/components/header.jsx
+++ app/javascript/mastodon/features/ui/components/header.jsx
@@ -108,7 +108,7 @@ class Header extends PureComponent {
       <div className='ui__header'>
         <Link to='/' className='ui__header__logo'>
           <WordmarkLogo />
-          <SymbolLogo />
+   
         </Link>
 
         <div className='ui__header__links'>
