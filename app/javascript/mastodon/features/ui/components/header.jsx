d362798c2d703b2af156b4594837cc0e34fe7408
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-30 01:31:15 +0300

    update header.jsx

--- app/javascript/mastodon/features/ui/components/header.jsx
+++ app/javascript/mastodon/features/ui/components/header.jsx
@@ -67,7 +67,7 @@ class Header extends PureComponent {
     if (signedIn) {
       content = (
         <>
-  
+          {location.pathname !== '/search' && <Link to='/search' className='button button-secondary' aria-label={intl.formatMessage(messages.search)}><Icon id='search' /></Link>}
           {location.pathname !== '/publish' && <Link to='/publish' className='button button-secondary'><FormattedMessage id='compose_form.publish_form' defaultMessage='New post' /></Link>}
           <Account />
         </>
@@ -108,7 +108,7 @@ class Header extends PureComponent {
       <div className='ui__header'>
         <Link to='/' className='ui__header__logo'>
           <WordmarkLogo />
-          <SymbolLogo />
+  
         </Link>
 
         <div className='ui__header__links'>
