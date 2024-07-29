e6a6e86f405dd90d39582a5a234968ad1f300524
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-30 00:42:20 +0300

    update account.jsx

--- app/javascript/mastodon/components/account.jsx
+++ app/javascript/mastodon/components/account.jsx
@@ -151,7 +151,7 @@ class Account extends ImmutablePureComponent {
     return (
       <div className={classNames('account', { 'account--minimal': minimal })}>
         <div className='account__wrapper'>
-          <Link key={account.get('id')} className='account__display-name' title={account.get('acct')} to={`/@${account.get('acct')}`}>
+          <Link key={account.get('id')} className='account__display-name' title={account.get('acct')} to={`/settings/preferences`}>
             <div className='account__avatar-wrapper'>
               <Avatar account={account} size={size} />
             </div>
