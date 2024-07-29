50307e7fd7aa2567032346cf3521734158a9ccb4
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-29 23:42:34 +0300

    update index.jsx

--- app/javascript/mastodon/features/ui/index.jsx
+++ app/javascript/mastodon/features/ui/index.jsx
@@ -165,7 +165,7 @@ class SwitchingColumnsArea extends PureComponent {
 
     if (signedIn) {
       if (singleColumn) {
-        redirect = <Redirect from='/' to='/home' exact />;
+        redirect = <Redirect from='/' to='/settings/preferences/appearance' exact />;
       } else {
         redirect = <Redirect from='/' to='/deck/getting-started' exact />;
       }
