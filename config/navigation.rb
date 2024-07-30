1fcf9c3f04c961e2741450ffbf85709ffc1064ab
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-30 05:35:28 +0300

    update navigation.rb

--- config/navigation.rb
+++ config/navigation.rb
@@ -4,6 +4,8 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |n|
     n.item :web, safe_join([fa_icon('chevron-left fw'), t('settings.back')]), root_path
 
+    n.item :other, safe_join([fa_icon('cog fw'), t('preferences.other')]), settings_preferences_other_path
+
     n.item :software_updates, safe_join([fa_icon('exclamation-circle fw'), t('admin.critical_update_pending')]), admin_software_updates_path, if: -> { ENV['UPDATE_CHECK_URL'] != '' && current_user.can?(:view_devops) && SoftwareUpdate.urgent_pending? }, html: { class: 'warning' }
 
     n.item :profile, safe_join([fa_icon('user fw'), t('settings.profile')]), settings_profile_path, if: -> { current_user.functional? }, highlights_on: %r{/settings/profile|/settings/featured_tags|/settings/verification|/settings/privacy}
