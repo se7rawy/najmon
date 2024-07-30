9a9c7c7189a7a75ab4ed77a92afd713c256bf652
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-30 09:01:13 +0300

    update header.jsx

--- app/javascript/mastodon/features/ui/components/header.jsx
+++ app/javascript/mastodon/features/ui/components/header.jsx
@@ -69,10 +69,10 @@ class Header extends PureComponent {
         <>
           {location.pathname !== '/search' && <Link to='/search' className='button button-secondary' aria-label={intl.formatMessage(messages.search)}><Icon id='search' /></Link>}
           {location.pathname !== '/publish' && <Link to='/publish' className='button button-secondary'><FormattedMessage id='compose_form.publish_form' defaultMessage='New post' /></Link>}
-     /*
+     {/*
         {location.pathname !== '/publish' && <Link to='/publish' className='button button-secondary'><Icon id='pencil' /></Link>}
           <a href='/settings/preferences' className='button button-secondary'> <Icon id='cog' /></a>
-     */
+     */}
           <Account />
   </>
       );
