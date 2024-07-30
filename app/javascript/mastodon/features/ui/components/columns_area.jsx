c03ad001c8eeecd8c327554841a102a2e3e8470a
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-30 09:38:33 +0300

    update columns_area.jsx

--- app/javascript/mastodon/features/ui/components/columns_area.jsx
+++ app/javascript/mastodon/features/ui/components/columns_area.jsx
@@ -156,7 +156,10 @@ export default class ColumnsArea extends ImmutablePureComponent {
             </div>
           </div>
 
-   
+          <div className='columns-area__panels__main'>
+      
+            <div className='columns-area columns-area--mobile'>{children}</div>
+          </div>
 
     
         </div>
