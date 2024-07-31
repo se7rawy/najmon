716143118a2aac688feb0cdd87087570e894ce9f
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-07-31 12:11:20 +0300

    update icon.tsx

--- app/javascript/mastodon/components/icon.tsx
+++ app/javascript/mastodon/components/icon.tsx
@@ -14,7 +14,7 @@ export const Icon: React.FC<Props> = ({
   ...other
 }) => (
   <i
-    className={classNames('fa', `fa-light fa-${id}`, className, { 'fa-fw': fixedWidth })}
+    className={classNames('fa', `fa-${id}`, className, { 'fa-fw': fixedWidth })}
     {...other}
   />
 );
