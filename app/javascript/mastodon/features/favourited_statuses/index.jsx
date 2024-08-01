0630d3c75ee40d91b10f6d7b0177aee4251fe280
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-08-02 01:22:28 +0300

    update index.jsx

--- app/javascript/mastodon/features/favourited_statuses/index.jsx
+++ app/javascript/mastodon/features/favourited_statuses/index.jsx
@@ -79,7 +79,7 @@ class Favourites extends ImmutablePureComponent {
     return (
       <Column bindToDocument={!multiColumn} ref={this.setRef} label={intl.formatMessage(messages.heading)}>
         <ColumnHeader
-          icon='star'
+          icon='heart'
           title={intl.formatMessage(messages.heading)}
           onPin={this.handlePin}
           onMove={this.handleMove}
