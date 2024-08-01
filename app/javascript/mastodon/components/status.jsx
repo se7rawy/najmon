dc25018f393329ba883f6f9f7fa8776852876d2f
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-08-01 07:13:19 +0300

    update status.jsx

--- app/javascript/mastodon/components/status.jsx
+++ app/javascript/mastodon/components/status.jsx
@@ -501,7 +501,7 @@ class Status extends ImmutablePureComponent {
           </Bundle>
         );
       } else {
-      {/*  media = (
+        media = (
           <Bundle fetchComponent={MediaGallery} loading={this.renderLoadingMediaGallery}>
             {Component => (
               <Component
@@ -517,17 +517,17 @@ class Status extends ImmutablePureComponent {
               />
             )}
           </Bundle>
-        );*/}
+        );
       }
     } else if (status.get('spoiler_text').length === 0 && status.get('card')) {
-      media = (
+     {/* media = (
         <Card
           onOpenMedia={this.handleOpenMedia}
           card={status.get('card')}
           compact
           sensitive={status.get('sensitive')}
         />
-      );
+      );*/}
     }
 
     if (account === undefined || account === null) {
